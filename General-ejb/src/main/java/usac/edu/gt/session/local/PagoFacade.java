8e74ca500f5d80d98c016873886dda28ab622189
Sandra Jimenez <sandraeujim@gmail.com>
2021-02-27 17:10:36 -0600

    R10B Bugfix, imprimir el total antes de retorno
    Se modifico el metodo listaproductos_sumatotal para imprimir el total antes de retornar

--- General-ejb/src/main/java/usac/edu/gt/session/local/PagoFacade.java
+++ General-ejb/src/main/java/usac/edu/gt/session/local/PagoFacade.java
@@ -49,5 +49,15 @@ public class PagoFacade extends AbstractFacade<Pago> implements PagoFacadeLocal{
 	public void sumarTotalPago(double total,Pago pago) {
 		pago.total += total;
 	}
+
+	public double listaproduct_sumatotal() {
+		ArrayList<Pago> lista_productos;
+		Double total = 0;	
+		for(int i = 0; i < lista_productos.size(); i++) {
+			total += lista_productos.get(i).total;
+		}
+		DoLogger.log().info("Total: " + total);
+		return total;
+	}
 	
 }
