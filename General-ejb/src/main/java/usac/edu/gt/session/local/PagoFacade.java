cc435e4d95f4f8010febb75784fdbdf1fa2bc5b3
Aaron Juarez <2963595050101@ingenieria.usac.edu.gt>
2021-02-27 16:36:25 -0600

    Requerimiento 8, Crear una funcion pago
    Se creó una función para pago: recibe un double y se suma el 10%

---
+++ General-ejb/src/main/java/usac/edu/gt/session/local/PagoFacade.java
@@ -0,0 +1,30 @@
+package usac.edu.gt.session.local;
+
+@Stateless
+public class PagoFacade extends AbstractFacade<Pago> implements PagoFacadeLocal{
+	
+	@PersistanceContect(unitName ='BD_PU')
+	private EntityManager em;
+	
+	@Override
+	protected EntityManager getEntityManager() {
+		
+		return em;
+	}
+	
+	@Resource
+	private EJBContext ejbContext;
+	
+	public PagoFacade() {
+		super(Pago.class);
+		
+	}
+	
+	@Override
+	public double pago1(double num1) {
+		double numReturn=num1+num1*0.1;
+		
+		return numReturn;
+	}
+	
+}
